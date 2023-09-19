f2b6b1800ecdc324753eddc8c3dd52f1b9314bf8
ishnoopy <aldringuasa@gmail.com>
2023-09-20 02:21:53 +0800

    Answered exercises for TDD.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(word, numberOfRepetition) {
+  if (numberOfRepetition < 0) return 'ERROR';
+  
+  let joinedWords = '';
+  for (let index = 0; index < numberOfRepetition; index++) {
+    joinedWords+=word;
+  }
 
+  return joinedWords;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(numbersArray, ...numbersToRemove) {
+  for (const numberToRemove of numbersToRemove){
+    // To check if number is in the array.
+    if (numbersArray.indexOf(numberToRemove) !== -1) {
+       let indexOfRemovedNumber = numbersArray.indexOf(numberToRemove)
+      // To check if number and element of array are the same datatype.
+       if( numberToRemove === numbersArray[indexOfRemovedNumber]) {
+        numbersArray.splice(indexOfRemovedNumber, 1)
+       }
+    }
+  }
 
+  return numbersArray;
 };
 
 // Do not edit below this line
