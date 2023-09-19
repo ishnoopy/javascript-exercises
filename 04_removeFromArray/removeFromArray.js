2a8e45fea48480fbf2ef04c1c0a2e499e396c2bd
ishnoopy <aldringuasa@gmail.com>
2023-09-20 02:41:53 +0800

    Answered Exercises for TDD.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,16 +1,19 @@
 const removeFromArray = function(numbersArray, ...numbersToRemove) {
-  for (const numberToRemove of numbersToRemove){
-    // To check if number is in the array.
-    if (numbersArray.indexOf(numberToRemove) !== -1) {
-       let indexOfRemovedNumber = numbersArray.indexOf(numberToRemove)
-      // To check if number and element of array are the same datatype.
-       if( numberToRemove === numbersArray[indexOfRemovedNumber]) {
-        numbersArray.splice(indexOfRemovedNumber, 1)
-       }
-    }
-  }
+  // for (const numberToRemove of numbersToRemove){
+  //   // To check if number is in the array.
+  //   if (numbersArray.indexOf(numberToRemove) !== -1) {
+  //      let indexOfRemovedNumber = numbersArray.indexOf(numberToRemove)
+  //     // To check if number and element of array are the same datatype.
+  //      if( numberToRemove === numbersArray[indexOfRemovedNumber]) {
+  //       numbersArray.splice(indexOfRemovedNumber, 1)
+  //      }
+  //   }
+  // }
 
-  return numbersArray;
+  // return numbersArray;
+
+  return numbersArray.filter((number) => !numbersToRemove.includes(number));
+
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+  if (typeof firstNumber !== "number" || typeof secondNumber !== "number" || firstNumber < 0 || secondNumber < 0) return "ERROR";
+  if (firstNumber > secondNumber) {
+    const placeholderNumber = firstNumber;
+    firstNumber = secondNumber;
+    secondNumber = placeholderNumber;
+  }
+  
+  let sum = 0;
+  for (let number = firstNumber; number <= secondNumber; number++){
+    sum += number;
+  }
 
+  return sum;
 };
 
 // Do not edit below this line
