a14a778f6e6eb3306dbdef90b0cb1e2480b1ec41
ishnoopy <aldringuasa@gmail.com>
2023-09-21 02:08:22 +0800

    Answered Exercises for TDD.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(augend, addend) {
+	return augend + addend;
 };
 
-const subtract = function() {
-	
+const subtract = function(minuend, subtrahend) {
+	return minuend - subtrahend;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+	for (const number of array) {
+    sum+=number;
+  }
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  // let result = array[0];
+  // for (let index = 1; index < array.length; index++) {
+  //   result *= array[index]
+  //   console.log(result)
+  // }
 
+  // return result;
+ 
+  return array.reduce((result, element) => result * element, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  let result = 1;
+  for (let currentExponent = 1; currentExponent <= exponent; currentExponent++) {
+    result *= base;
+  }
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let result = 1;
+  for (let index = number; index > 0; index--) {
+    result *= index;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  
+  const filteredString = string.toLowerCase().replace(/[^a-z0-9]/g,"");
+  return filteredString.split("").reverse().join("") == filteredString;
+  
 };
 
 // Do not edit below this line
