36baa2d864a69148bb1366742d87128141c7b247
ishnoopy <aldringuasa@gmail.com>
2023-09-21 14:04:16 +0800

    Answered Exercises for TDD.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+  if(+index < 0 ) return 'OOPS';
 
+  let array = [0,1];
+
+  for (let i = 0; i <= +index; i++ ) {
+    if (i >= 2)  {
+      array.push(array[i-2] + array[i-1])
+    }
+  }
+  
+  return array[+index]
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
+  for (const person of peopleArray) {
+    if (!person.yearOfDeath) {
+      person["yearOfDeath"] = new Date().getFullYear();
+    }
+    person["age"] = person.yearOfDeath - person.yearOfBirth;
+  }
 
+  const oldestPerson = peopleArray.reduce((maxPerson, person) => {
+    if (person.age > maxPerson.age) {
+      return person;
+    }
+
+    return maxPerson;
+
+  }, peopleArray[0])
+
+  return oldestPerson;
 };
 
 // Do not edit below this line
